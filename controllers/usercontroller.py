2147c1cc944eb39a0171d013f451134f9c5fd953
Andreas Pedersen <and@le34.dk>
2018-12-18 23:48:22 +0100

    Comments and cleans all lifecycle files

--- controllers/usercontroller.py
+++ controllers/usercontroller.py
@@ -1,11 +1,13 @@
 class UserController:
+
+    #################################################### setup
     def __init__(self, user_store, user_view):
         self.user_store = user_store
         self.user_view = user_view
 
+    #################################################### Functionality
     def login(self, username, password):
         return self.user_store.login(username,password)
-        
 
     def signup(self, username, password):
         return self.user_store.signup(username,password)
