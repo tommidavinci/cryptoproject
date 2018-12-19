f03cd63948e75020f7d85c2514525b17436fd8ef
DaMasterHam <cmeisterham@gmail.com>
2018-12-19 06:03:05 +0100

    Changes hashing construction used from argon2i to argon2id

--- kdf.py
+++ kdf.py
@@ -1,13 +1,18 @@
 from nacl import pwhash, utils
 
 #################################################### Setup
-size = pwhash.argon2i.BYTES_MIN
+size = pwhash.argon2id.BYTES_MIN
 
 #################################################### Hash by Key-Derivation
-def hash_password(password, salt = utils.random(pwhash.argon2i.SALTBYTES)):
-    hashed_password = pwhash.argon2i.kdf(size, bytes(password, 'utf-8'), salt)
+def hash_password(password, salt = utils.random(pwhash.argon2id.SALTBYTES)):
+    hashed_password = pwhash.argon2id.kdf(size, bytes(password, 'utf-8'), salt)
     return (hashed_password, salt)
 
 #################################################### Verify Password match
 def verify_password(password, salt, hashed_password):
     return hash_password(password, salt)[0] == hashed_password
+
+# p = b'password'
+# h = b'("\205#k|\342_\327Df\322\211\256\342\024'
+
+# print(pwhash.argon2i.verify(h,p))
