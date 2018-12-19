fa376548694a454440f2d8dce4816283d14a1986
DaMasterHam <cmeisterham@gmail.com>
2018-12-19 09:35:12 +0100

    Adds template database.ini file and changes hash to argon2id

--- kdf.py
+++ kdf.py
@@ -5,8 +5,8 @@ size = pwhash.argon2id.BYTES_MIN
 
 #################################################### Hash by Key-Derivation
 def hash_password(password, salt = utils.random(pwhash.argon2id.SALTBYTES)):
-    hashed_password = pwhash.argon2id.kdf(size, bytes(password, 'utf-8'), salt)
-    return (hashed_password, salt)
+    hashed = pwhash.argon2id.kdf(size, bytes(password, 'utf-8'), salt)
+    return (hashed, salt)
 
 #################################################### Verify Password match
 def verify_password(password, salt, hashed_password):
